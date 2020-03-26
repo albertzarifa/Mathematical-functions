784a1167c9f9f1497cb79b0881bf8abae4343167
Albert Zarifa <albert.zarifa@gmail.com>
2020-03-26 01:21:40 -0400

    This commit is for the new "is_prime" function.

--- mathematical-functions.py
+++ mathematical-functions.py
@@ -17,3 +17,29 @@ def euclidean_algorithm():
         b = r
         r = a%b
     print("The gcd is: ", b)
+
+
+def is_prime(n):
+    """
+    This function return whether a given number is prime or not.
+    >>> is_prime(3)
+    True
+    >>> is_prime(2)
+    True
+    >>> is_prime(7)
+    False
+    """
+
+    num_check = 1
+    num_of_divisors = 0
+
+    if n <= 1:                   # This will automatically return False if the given number
+        return False             # is less than or eqaual to 1, and prime numbers start at 2.
+
+    while (num_check < n+1):     # To check all the numbers to and including n.
+        if n % num_check == 0:
+            num_of_divisors += 1 # Add one to the number of divisors so we can check the number we get later.
+        num_check += 1           # This is to go to the next number to check
+
+    return (num_of_divisors == 2)# This will only return True if the number of divisors is 2,
+                                 # where the 2 are only 1 and n. 
